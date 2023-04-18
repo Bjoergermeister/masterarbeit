81381a5c177ede74bbc7a5d8cf65ab017612b23b
Bjoergermeister <b.svensson@ostfalia.de>
2023-04-18 12:55:21 +0200

    [ADD] bash script to create the filesystem for the file open benchmark

---
+++ Filesystem/Open/create_filesystem.sh
@@ -0,0 +1,26 @@
+mkdir -p Filesystem/Regular Filesystem/Overlay
+
+# Create filesystem for ext4
+mkdir -p Filesystem/Regular/dir
+pushd Filesystem/Regular/dir > /dev/null
+
+for i in {0..15}
+do
+    touch $i.txt
+    dd if=/dev/urandom of=./$i.txt count=1 status=none
+done
+popd > /dev/null
+
+# Create filesystem for overlay
+mkdir -p Filesystem/Overlay
+pushd Filesystem/Overlay > /dev/null
+
+mkdir upper workdir merged
+
+for i in {0..15}
+do 
+    mkdir -p Layer$i/dir
+    touch Layer$i/dir/$i.txt
+    dd if=/dev/urandom of=Layer$i/dir/$i.txt count=1 status=none
+done
+popd > /dev/null
