7af27672282b09d255c5ee83519ef0780758e601
Bjoergermeister <b.svensson@ostfalia.de>
2023-04-20 17:23:52 +0200

    [ADD] measurement program and support files for read() in C

---
+++ Filesystem/Read/C/read.c
@@ -0,0 +1,37 @@
+#include <time.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "../../../common/C/timing.h"
+#include "../../../common/C/benchmark.h"
+
+#define BLOCK_SIZE 512
+
+int main(int argc, char **argv)
+{
+    char *file_to_read = argv[1];
+    char *file_to_save = argv[2];
+
+    struct timespec start;
+    struct timespec end;
+
+    int fd = open(file_to_read, O_RDONLY | __O_DIRECT, S_IRWXU);
+    if (handle_error(fd, file_to_read))
+        return -1;
+
+    int file_size = lseek(fd, 0, SEEK_END);
+
+    char *read_buffer = malloc(sizeof(char) * file_size);
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
+    read(fd, read_buffer, file_size);
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
+
+    if (handle_error(close(fd), file_to_read))
+        return -1;
+
+    long difference = calculate_time_difference(&start, &end);
+
+    return save_benchmark_result(difference, file_to_save);
+}
