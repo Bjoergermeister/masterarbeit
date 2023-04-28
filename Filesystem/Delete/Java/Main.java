a4a4bb865d0f94a5182b068656b1ce9f24bfff6c
Bjoergermeister <b.svensson@ostfalia.de>
2023-04-28 15:22:29 +0200

    [ADD] measurement program for file deletion in Java

---
+++ Filesystem/Delete/Java/Main.java
@@ -0,0 +1,38 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Zu wenig Argumente!");
+            System.exit(-1);
+        }
+
+        String newFile = args[0];
+        String saveFile = args[1];
+
+        File file = new File(newFile);
+
+        long start = System.nanoTime();
+        file.delete();
+        long end = System.nanoTime();
+
+        long result = end - start;
+        save(saveFile, result);
+    }
+
+    public static void save(String saveFile, long result) {
+        try {
+            FileWriter fileWriter = new FileWriter(saveFile, true);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            bufferedWriter.append(Long.toString(result) + "\n");
+            bufferedWriter.close();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
--- Filesystem/Delete/run.sh
+++ Filesystem/Delete/run.sh
@@ -13,7 +13,9 @@ function regular() {
     
     for i in {0..99}
     do
-        
+        echo "$i in 99"
+
+        # C version
         pushd Filesystem/Regular > /dev/null
         
         ../../C/delete "dir/1.txt" "${result_dir}/Filesystem_Delete_C_Regular_1.txt"
@@ -22,6 +24,19 @@ function regular() {
         ../../C/delete "dir/512.txt" "${result_dir}/Filesystem_Delete_C_Regular_512.txt"
         ../../C/delete "dir/1024.txt" "${result_dir}/Filesystem_Delete_C_Regular_1024.txt"
         ../../C/delete "dir/2048.txt" "${result_dir}/Filesystem_Delete_C_Regular_2048.txt"
+        
+        popd > /dev/null
+        reset_filesystem
+
+        # Java version
+        pushd Filesystem/Regular > /dev/null
+        
+        java -cp ../../Java Main "dir/1.txt" "${result_dir}/Filesystem_Delete_Java_Regular_1.txt"
+        java -cp ../../Java Main "dir/128.txt" "${result_dir}/Filesystem_Delete_Java_Regular_128.txt"
+        java -cp ../../Java Main "dir/256.txt" "${result_dir}/Filesystem_Delete_Java_Regular_256.txt"
+        java -cp ../../Java Main "dir/512.txt" "${result_dir}/Filesystem_Delete_Java_Regular_512.txt"
+        java -cp ../../Java Main "dir/1024.txt" "${result_dir}/Filesystem_Delete_Java_Regular_1024.txt"
+        java -cp ../../Java Main "dir/2048.txt" "${result_dir}/Filesystem_Delete_Java_Regular_2048.txt"
         
         popd > /dev/null
         reset_filesystem
@@ -34,7 +49,11 @@ function manual() {
 
     for i in {0..99}
     do
+        echo "$i in 99"
+
         result_dir="../../../../Results"
+
+        # C version
         reset_filesystem
 
         cd Filesystem
@@ -51,6 +70,24 @@ function manual() {
         cd ../..
         sudo umount overlay
         rm -rf Filesystem/upper/dir/*
+
+        # Java version
+        reset_filesystem
+
+        cd Filesystem
+        sudo mount -t overlay overlay -o lowerdir=Regular,upperdir=upper,workdir=workdir merged
+        cd merged
+    
+        java -cp ../../Java Main "dir/1.txt" "${result_dir}/Filesystem_Delete_Java_Manual_1.txt"
+        java -cp ../../Java Main "dir/128.txt" "${result_dir}/Filesystem_Delete_Java_Manual_128.txt"
+        java -cp ../../Java Main "dir/256.txt" "${result_dir}/Filesystem_Delete_Java_Manual_256.txt"
+        java -cp ../../Java Main "dir/512.txt" "${result_dir}/Filesystem_Delete_Java_Manual_512.txt"
+        java -cp ../../Java Main "dir/1024.txt" "${result_dir}/Filesystem_Delete_Java_Manual_1024.txt"
+        java -cp ../../Java Main "dir/2048.txt" "${result_dir}/Filesystem_Delete_Java_Manual_2048.txt"
+    
+        cd ../..
+        sudo umount overlay
+        rm -rf Filesystem/upper/dir/*
     done
 
 }
@@ -62,17 +99,28 @@ function container() {
     pushd ../.. > /dev/null
 
     volume="type=bind,source=$(pwd)/Results,target=/Results"
-    image="masterarbeit-filesystem-delete-c"
-    output_file_prefix="Results/Filesystem_Delete_C_Container_"
+    c_image="masterarbeit-filesystem-delete-c"
+    java_image="masterarbeit-filesystem-delete-java"
+    c_prefix="Results/Filesystem_Delete_C_Container_"
+    java_prefix="Results/Filesystem_Delete_Java_Container_"
     
     for i in {0..99}
     do
-        docker run --name "$image" -it --rm --mount "$volume" "$image" ./delete "dir/1.txt" "${output_file_prefix}1.txt"
-        docker run --name "$image" -it --rm --mount "$volume" "$image" ./delete "dir/128.txt" "${output_file_prefix}128.txt"
-        docker run --name "$image" -it --rm --mount "$volume" "$image" ./delete "dir/256.txt" "${output_file_prefix}256.txt"
-        docker run --name "$image" -it --rm --mount "$volume" "$image" ./delete "dir/512.txt" "${output_file_prefix}512.txt"
-        docker run --name "$image" -it --rm --mount "$volume" "$image" ./delete "dir/1024.txt" "${output_file_prefix}1024.txt"
-        docker run --name "$image" -it --rm --mount "$volume" "$image" ./delete "dir/2048.txt" "${output_file_prefix}2048.txt"
+        echo "$i in 99"
+        docker run --name "$c_image" -it --rm --mount "$volume" "$c_image" ./delete "dir/1.txt" "${c_prefix}1.txt"
+        docker run --name "$c_image" -it --rm --mount "$volume" "$c_image" ./delete "dir/128.txt" "${c_prefix}128.txt"
+        docker run --name "$c_image" -it --rm --mount "$volume" "$c_image" ./delete "dir/256.txt" "${c_prefix}256.txt"
+        docker run --name "$c_image" -it --rm --mount "$volume" "$c_image" ./delete "dir/512.txt" "${c_prefix}512.txt"
+        docker run --name "$c_image" -it --rm --mount "$volume" "$c_image" ./delete "dir/1024.txt" "${c_prefix}1024.txt"
+        docker run --name "$c_image" -it --rm --mount "$volume" "$c_image" ./delete "dir/2048.txt" "${c_prefix}2048.txt"
+    
+        # Java version
+        docker run --name "$java_image" -it --rm --mount "$volume" "$java_image" java Main "dir/1.txt" "${java_prefix}1.txt"
+        docker run --name "$java_image" -it --rm --mount "$volume" "$java_image" java Main "dir/128.txt" "${java_prefix}128.txt"
+        docker run --name "$java_image" -it --rm --mount "$volume" "$java_image" java Main "dir/256.txt" "${java_prefix}256.txt"
+        docker run --name "$java_image" -it --rm --mount "$volume" "$java_image" java Main "dir/512.txt" "${java_prefix}512.txt"
+        docker run --name "$java_image" -it --rm --mount "$volume" "$java_image" java Main "dir/1024.txt" "${java_prefix}1024.txt"
+        docker run --name "$java_image" -it --rm --mount "$volume" "$java_image" java Main "dir/2048.txt" "${java_prefix}2048.txt"
     done
 
     popd > /dev/null
