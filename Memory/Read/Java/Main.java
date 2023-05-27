31f3be82801e37409e9e2cae0bc09aeb4f3c5e51
Bjoergermeister <b.svensson@ostfalia.de>
2023-05-27 08:58:48 +0200

    [ADD] measurement programs for memory in Java

---
+++ Memory/Read/Java/Main.java
@@ -0,0 +1,66 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Main {
+
+    private static final int PAGE_SIZE = 4096;
+    private static final long THREE_HUNDRED_MEGABYTES = 1024 * 1024 * 300;
+    private static final ArrayList<byte[]> ARRAYS = new ArrayList<byte[]>();
+
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+        String filePrefix = args[0];
+
+        long timeSum = 0;
+
+        allocate_and_fill();
+
+        for (int i = ARRAYS.size(); i > 0; i--) {
+            int sum = 0;
+            long start = System.nanoTime();
+            byte[] array = ARRAYS.get(i);
+            for (int j = 0; j < PAGE_SIZE; j++) {
+                sum += array[j];
+            }
+            long end = System.nanoTime();
+
+            timeSum += end - start;
+
+            if (i % 600 == 0 && i < ARRAYS.size()) {
+                save(
+                        filePrefix,
+                        i / 600,
+                        timeSum / 600);
+                timeSum = 0;
+            }
+        }
+    }
+
+    static void allocate_and_fill() {
+        long total_allocated_memory = 0;
+        while (total_allocated_memory < THREE_HUNDRED_MEGABYTES) {
+            byte[] array = new byte[PAGE_SIZE];
+
+            for (int i = 0; i < 4096; i++) {
+                array[i] = 1;
+            }
+
+            ARRAYS.add(array);
+            total_allocated_memory += PAGE_SIZE;
+        }
+    }
+
+    static void save(String prefix, long count, long result) {
+        String fileName = String.format("%s_%d.txt", prefix, count);
+        try {
+            FileWriter fileWriter = new FileWriter(fileName, true);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            bufferedWriter.append(Long.toString(result) + "\n");
+            bufferedWriter.close();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
---
+++ Memory/Write/Java/Main.java
@@ -0,0 +1,56 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Main {
+
+    private static final int PAGE_SIZE = 4096;
+    private static final long THREE_HUNDRED_MEGABYTES = 1024 * 1024 * 300;
+
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+        String filePrefix = args[0];
+
+        long total_allocated_memory = 0;
+        long allocationCounter = 0;
+        long timeSum = 0;
+        ArrayList<byte[]> arrays = new ArrayList<byte[]>();
+
+        while (total_allocated_memory < THREE_HUNDRED_MEGABYTES) {
+            byte[] array = new byte[PAGE_SIZE];
+
+            long start = System.nanoTime();
+            for (int i = 0; i < 4096; i++) {
+                array[i] = 1;
+            }
+            long end = System.nanoTime();
+
+            timeSum += (end - start);
+
+            total_allocated_memory += 4096;
+            allocationCounter++;
+            arrays.add(array);
+
+            if (allocationCounter % 600 == 0 && allocationCounter > 0) {
+                save(
+                        filePrefix,
+                        allocationCounter / 600,
+                        timeSum / 600);
+                timeSum = 0;
+            }
+        }
+    }
+
+    public static void save(String prefix, long count, long result) {
+        String fileName = String.format("%s_%d.txt", prefix, count);
+        try {
+            FileWriter fileWriter = new FileWriter(fileName, true);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            bufferedWriter.append(Long.toString(result) + "\n");
+            bufferedWriter.close();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
