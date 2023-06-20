b2b442fbbdfe007c553fb009ef6371cb732816df
Bjoergermeister <b.svensson@ostfalia.de>
2023-06-20 17:33:23 +0200

    [ADD] measurement program for network throughput in C

---
+++ Network/Throughput/C/client.c
@@ -0,0 +1,80 @@
+// Global defines
+#define _DEFAULT_SOURCE 1
+
+#include <ifaddrs.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+
+#include "../../../common/C/timing.h"
+#include "../../../common/C/benchmark.h"
+#include "../../../common/C/network.h"
+
+#define BYTES_IN_ONE_GIGABYTE (1024 * 1024 * 1024)
+
+float process_response(char *response)
+{
+    long long throughput_as_ll = strtoll(response, NULL, 10);
+    return *(float *)&throughput_as_ll;
+}
+
+int main(int argc, char **argv)
+{
+    char *source_ip_address = argv[1];
+    char *destination_ip_address = argv[2];
+    char *save_filename = argv[3];
+
+    struct timespec start;
+    struct timespec end;
+
+    struct sockaddr_in destination_address;
+    configure_sockaddr(&destination_address, destination_ip_address, 3000);
+
+    int socket = open_socket(SOCK_DGRAM, IPPROTO_UDP);
+
+    struct sockaddr *dest = (struct sockaddr *)&destination_address;
+    socklen_t dest_length = sizeof(struct sockaddr *);
+
+    char buffer[1460];
+    memset(buffer, 'A', 1460);
+
+    int send_count = 0;
+    long total_bytes_send = 0;
+    while (total_bytes_send < BYTES_IN_ONE_GIGABYTE)
+    {
+        if ((BYTES_IN_ONE_GIGABYTE - total_bytes_send) <= sizeof(buffer))
+        {
+            memset(buffer, '\0', 8);
+        }
+
+        int bytes_send = sendto(socket, buffer, sizeof(buffer), 0, dest, sizeof(struct sockaddr));
+        if (bytes_send == -1)
+        {
+            perror("sendto(): ");
+            exit(-1);
+        }
+
+        send_count++;
+        total_bytes_send += bytes_send;
+    }
+
+    printf("Send %ld bytes (%d packets)\n", total_bytes_send, send_count);
+
+    // Receive response from server which contains the result
+    size_t buffer_size = 100; // Long enough for a float
+    char response[buffer_size];
+    int bytes_received = recvfrom(socket, response, buffer_size, 0, dest, &dest_length);
+    if (bytes_received == -1)
+    {
+        perror("recvfrom(): ");
+        exit(-1);
+    }
+
+    float result = process_response(response);
+    save_benchmark_result_float(result, save_filename);
+}
---
+++ Network/Throughput/C/server.c
@@ -0,0 +1,117 @@
+// Global defines
+#define _DEFAULT_SOURCE 1
+
+#include <ifaddrs.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+
+#include "../../../common/C/timing.h"
+#include "../../../common/C/benchmark.h"
+#include "../../../common/C/network.h"
+
+#define STOP_FLAG "\0\0\0\0\0\0\0\0"
+#define STOP_FLAG_LENGTH 8
+#define BYTES_IN_ONE_MEGABYTE (1024 * 1024)
+
+float calculate_throughput(long bytes, long time_difference)
+{
+    float time_in_seconds = (float)time_difference / NANOSECONDS_IN_ONE_SECOND;
+    float megabytes = (float)bytes / BYTES_IN_ONE_MEGABYTE;
+    printf("%f megabytes in %f seconds = %f mb/s\n", megabytes, time_in_seconds, megabytes / time_in_seconds);
+    return megabytes / time_in_seconds;
+}
+
+void write_response(char *buffer, long bytes, long time_difference)
+{
+    float throughput = calculate_throughput(bytes, time_difference);
+    long throughput_as_int = *(long *)&throughput;
+
+    sprintf(buffer, "%ld\n", throughput_as_int);
+}
+
+int main(int argc, char **argv)
+{
+    char *server_ip_address = argv[1];
+    char *client_ip_address = argv[2];
+    char *save_filename = argv[3];
+
+    struct timespec start;
+    struct timespec end;
+
+    struct sockaddr_in server_address;
+    configure_sockaddr(&server_address, server_ip_address, 3000);
+
+    struct sockaddr_in client_address;
+    configure_sockaddr(&client_address, client_ip_address, 3000);
+
+    int socket = open_socket(SOCK_DGRAM, IPPROTO_UDP);
+
+    int new_size = 1024 * 1024 * 16;
+    handle_error(setsockopt(socket, SOL_SOCKET, SO_RCVBUF, &new_size, sizeof(int)), "setsockopt(): ");
+
+    struct sockaddr *server = (struct sockaddr *)&server_address;
+    handle_error(bind(socket, server, sizeof(struct sockaddr)), "bind(): ");
+
+    struct sockaddr *client = (struct sockaddr *)&client_address;
+    socklen_t length = sizeof(struct sockaddr);
+
+    char client_message[1500];
+
+    while (true)
+    {
+        long total_bytes_received = 0;
+        int receive_count = 0;
+        bool is_first_packet = true;
+
+        while (true)
+        {
+            int bytes_received = recvfrom(socket, client_message, 1500, 0, client, &length);
+            if (bytes_received < 0)
+            {
+                perror("recvfrom(): ");
+                exit(-1);
+            }
+
+            if (is_first_packet)
+            {
+                is_first_packet = false;
+                clock_gettime(CLOCK_MONOTONIC, &start);
+            }
+
+            receive_count++;
+            total_bytes_received += bytes_received;
+
+            if (strncmp(STOP_FLAG, client_message, STOP_FLAG_LENGTH) == 0)
+                break;
+        }
+
+        clock_gettime(CLOCK_MONOTONIC, &end);
+
+        long difference = calculate_time_difference(&start, &end);
+
+        if (receive_count != 735440)
+        {
+            printf("Missing packets!\n");
+        }
+        // Send response with time to client
+        size_t buffer_size = 100;
+        char response[buffer_size];
+        write_response(response, total_bytes_received, difference);
+
+        int result = sendto(socket, response, buffer_size, 0, client, length);
+        if (result == -1)
+        {
+            perror("sendto(): ");
+            exit(-1);
+        }
+    }
+
+    close(socket);
+}
