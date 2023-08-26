5df47107166dcb0114a543443513ce7f16cd6af2
Bjoergermeister <b.svensson@ostfalia.de>
2023-08-26 09:02:19 +0200

    [UPDATE] container/vm makefile and run script

--- ContainerVM/run.sh
+++ ContainerVM/run.sh
@@ -1,20 +1,28 @@
 function regular(){
+    echo "Running experiments in $1 mode"
+
     for i in {0..99}
     do
         echo "$((i + 1)) von 100"
-        ./GameOfLife/gameoflife "GameOfLife_$1.txt"
-        ./qsort/qsort "Qsort_$1.txt"
+        ./GameOfLife/gameoflife "../Results/GameOfLife_$1.txt"
+        ./qsort/qsort "../Results/Qsort_$1.txt"
+        ./n-body/nbody 1000000 "../Results/Nbody_$1.txt"
+        ./binary-trees/binary-trees 22 "../Results/BinaryTrees_$1.txt"
     done
 }
 
 function container(){
+    echo "Running experiments in container mode"
+    
     base_image="masterarbeit-containervm"
     volume="type=bind,source=$(pwd)/../Results,target=/Results"
     for i in {0..99}
     do
-        echo "$i von 99"
-        docker run --name GameOfLife --rm --mount "$volume" $base_image-gameoflife ./gameoflife "GameOfLife_Container.txt" 
-        docker run --name Qsort --rm --mount "$volume" $base_image-qsort ./qsort "Qsort_Container.txt"                
+        echo "$((i + 1)) von 100"
+        docker run --name GameOfLife --rm --mount "$volume" $base_image-gameoflife ./gameoflife "Results/GameOfLife_Container.txt" 
+        docker run --name Qsort --rm --mount "$volume" $base_image-qsort ./qsort "/Results/Qsort_Container.txt"                
+        docker run --name Nbody --rm --mount "$volume" $base_image-nbody ./nbody 1000000 "/Results/Nbody_Container.txt"                
+        docker run --name BinaryTrees --rm --mount "$volume" $base_image-binarytrees ./binary-trees 22 "/Results/BinaryTrees_Container.txt"                
     done
 }
 
